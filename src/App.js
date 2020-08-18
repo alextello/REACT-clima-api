bf6a74b83fd2474af6a74773783d465e5e6e7b56
unknown <alextello1@hotmail.com>
2020-08-17 20:06:29 -0600

    verificando resultados

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
   const [busqueda, setBusqueda] = useState({
@@ -9,23 +10,36 @@ function App() {
     ciudad: ''
   });
 
-  const { ciudad, pais } = busqueda;
   const [consultar, setConsultar] = useState(false);
   const [resultado, setResultado] = useState({});
+  const [error, setError] = useState(false);
+  const { ciudad, pais } = busqueda;
   useEffect(() => {
     const consultarAPI = async () => {
-      const key = '8dd036fb578b44f2216e09e570fb2c11';
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${key}`;
-      const resp = await fetch(url);
-      const resultado = await resp.json();
-      setResultado(resultado);
+      if (consultar) {
+        const key = '8dd036fb578b44f2216e09e570fb2c11';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${key}`;
+        const resp = await fetch(url);
+        const resultado = await resp.json();
+        setResultado(resultado);
+        setConsultar(false);
+        if (resultado.cod === '404') {
+          setError(true);
+        } else {
+          setError(false);
+        }
+      }
     }
-    if (consultar) {
-      consultarAPI();
-      setConsultar(false);
-    }
-  }, [consultar, ciudad, pais])
 
+    consultarAPI();
+
+  }, [consultar, ciudad, pais, resultado])
+  let componente;
+  if (error) {
+    componente = < Error mensaje="No hay resultados" />
+  } else {
+    componente = <Clima resultado={resultado} />
+  }
   return (
     <Fragment>
       <Header titulo="React Clima" />
@@ -36,7 +50,7 @@ function App() {
               <Formulario busqueda={busqueda} setBusqueda={setBusqueda} setConsultar={setConsultar} />
             </div>
             <div className="col m6 s12">
-              <Clima resultado={resultado} />
+              {componente}
             </div>
           </div>
         </div>
