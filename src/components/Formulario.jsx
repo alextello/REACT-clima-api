c3e98351971c396394eeb3f1baba38ec0530a910
unknown <alextello1@hotmail.com>
2020-08-17 18:43:33 -0600

    Validacion de formulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import Error from './Error';
+const Formulario = () => {
+	// state del formulario
+
+	const [busqueda, setBusqueda] = useState({
+		ciudad: '',
+		pais: '',
+	});
+
+	// extraer ciudad y país
+	const { ciudad, pais } = busqueda;
+
+	const [error, setError] = useState(false);
+
+	// Funcion que coloca los elementos en el state
+	const handleChange = (e) => {
+		// actualizar state
+		setBusqueda({
+			...busqueda,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	// Manejando el submit
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Validar
+		if (ciudad.trim() === '' || pais.trim() === '') {
+			setError(true);
+			return;
+		}
+
+		setError(false);
+
+		// Pasar al componente principal
+	};
+
+	return (
+		<form onSubmit={handleSubmit}>
+			{error ? <Error mensaje="Faltan campos para realizar la consulta" /> : null}
+			<div className="input-field col s12">
+				<input
+					type="text"
+					name="ciudad"
+					id="ciudad"
+					value={ciudad}
+					onChange={handleChange}
+				/>
+				<label htmlFor="ciudad">Ciudad: </label>
+			</div>
+			<div className="input-field col s12">
+				<select name="pais" id="pais" value={pais} onChange={handleChange}>
+					<option value="">-- SELECCIONE PAIS --</option>
+					<option value="GT">Guatemala</option>
+					<option value="US">Estados Unidos</option>
+					<option value="MX">México</option>
+					<option value="AR">Argentina</option>
+					<option value="CO">Colombia</option>
+					<option value="CR">Costa Rica</option>
+					<option value="ES">España</option>
+					<option value="PE">Perú</option>
+				</select>
+				<label htmlFor="ciudad">País: </label>
+			</div>
+			<div className="input-field col s12">
+				<button
+					className="right-align waves-effect waves-light btn-large btn-block yellow accent-4"
+					type="submit"
+				>
+					Buscar
+				</button>
+			</div>
+		</form>
+	);
+};
+
+export default Formulario;
